da50b3a06fc98acc3ce647155787ea1a27982e84
Reuven <rh@tufin.com>
2020-04-19 13:26:16 +0300

    call image scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ main() {
   sanitize "${INPUT_SECURECLOUD_ACCOUNT}" securecloud_account
   sanitize "${INPUT_SECURECLOUD_PROJECT}" securecloud_project
   echo "hola action ${INPUT_SECURECLOUD_ACCOUNT}/${INPUT_SECURECLOUD_PROJECT}"
+  scanImage $INPUT_SECURECLOUD_ACCOUNT $INPUT_SECURECLOUD_PROJECT "tufinim/generic-bank" "latest"
 }
 
 sanitize() {
@@ -12,6 +13,19 @@ sanitize() {
     >&2 echo "Unable to find ${2}. Did you set with.${2}?"
     exit 1
   fi
+}
+
+scanImage() {
+  export TUFIN_DOMAIN=$1
+  export TUFIN_PROJECT=$2
+  export IMAGE_NAME=$3
+  export IMAGE_TAG=$4
+  export TUFIN_URL=https://securecloud.tufin.io
+  export TUFIN_API_KEY=secrets.GENERIC_BANK_RETAIL_ALL_TOKEN
+  export TUFIN_DOCKER_REPO_PASSWORD=secrets.GENERIC_BANK_RETAIL_AGENT_TOKEN
+  url="$TUFIN_URL/api/scripts/image-scan"
+  
+  bash <(curl -s $url) "$IMAGE_NAME:$IMAGE_TAG"  
 }
 
 main
