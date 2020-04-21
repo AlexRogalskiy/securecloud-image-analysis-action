e64747c2504bc2b34b13ea7c295dd5454080c704
Reuven <rh@tufin.com>
2020-04-21 17:42:38 +0300

    env -> with

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,27 @@
 set -e
 
 main() {
+  sanitize "${INPUT_SECURECLOUD_ACCOUNT}" securecloud_account
+  sanitize "${INPUT_SECURECLOUD_PROJECT}" securecloud_project
+  sanitize "${INPUT_IMAGE}" image
+  sanitize "${INPUT_TAG}" tag
+
+  scanImage "${INPUT_SECURECLOUD_ACCOUNT}" "${INPUT_SECURECLOUD_PROJECT}" "${INPUT_IMAGE}" "${INPUT_TAG}"
+}
+
+sanitize() {
+  if [ -z "${1}" ]; then
+    >&2 echo "Unable to find ${2}. Did you set with.${2}?"
+    exit 1
+  fi
+}
+
+scanImage() {
+  export TUFIN_DOMAIN=$1
+  export TUFIN_PROJECT=$2
+  export IMAGE_NAME=$3
+  export IMAGE_TAG=$4
+
   export TUFIN_URL="https://securecloud.tufin.io"
   url="$TUFIN_URL/api/scripts/image-scan"
 
