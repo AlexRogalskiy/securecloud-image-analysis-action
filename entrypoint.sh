fbbc2c774b6db723ced33c9830871cb5fc4b0358
Reuven <rh@tufin.com>
2020-04-21 12:20:30 +0300

    build docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,28 +2,8 @@
 set -e
 
 main() {
-  sanitize "${INPUT_SECURECLOUD_ACCOUNT}" securecloud_account
-  sanitize "${INPUT_SECURECLOUD_PROJECT}" securecloud_project
-  sanitize "${INPUT_IMAGE_NAME}" image_name
-  sanitize "${INPUT_IMAGE_TAG}" image_tag
-  scanImage $INPUT_SECURECLOUD_ACCOUNT $INPUT_SECURECLOUD_PROJECT $INPUT_IMAGE_NAME $INPUT_IMAGE_TAG
-}
-
-sanitize() {
-  if [ -z "${1}" ]; then
-    >&2 echo "Unable to find ${2}. Did you set with.${2}?"
-    exit 1
-  fi
-}
-
-scanImage() {
-  export TUFIN_DOMAIN=$1
-  export TUFIN_PROJECT=$2
-  export IMAGE_NAME=$3
-  export IMAGE_TAG=$4
   export TUFIN_URL="https://securecloud.tufin.io"
   url="$TUFIN_URL/api/scripts/image-scan"
-  echo "test=$TEST"
 
   curl -s $url > scan.sh
   result="$?"
@@ -31,7 +11,7 @@ scanImage() {
       echo "Failed to retrieve scan script from $url with exit code $result"
       exit 1
   fi
-  bash -x scan.sh "$IMAGE_NAME:$IMAGE_TAG"  
+  bash scan.sh "$IMAGE_NAME:$IMAGE_TAG"  
 }
 
 main
